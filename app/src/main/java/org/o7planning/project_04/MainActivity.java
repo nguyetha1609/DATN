857cf8d8cc5d55589b17976066d2999f84f58509
Nguyệt Hà <nguyetha.1609@gmail.com>
2025-05-08 17:56:49 +0700

    Initial commit

---
+++ app/src/main/java/org/o7planning/project_04/MainActivity.java
@@ -0,0 +1,42 @@
+package org.o7planning.project_04;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        };
+    public void onBackPressed(){
+        //Tạo Dialog
+        new AlertDialog.Builder(this)
+                .setTitle("Thoát ứng dụng")
+                .setMessage("Bạn có chắc muốn thoát ứng dụng?")
+                .setIcon(R.drawable.ic_warning)
+                .setPositiveButton("Thoát", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // Khi user xác nhận, gọi finish() hoặc super.onBackPressed() để đóng Activity
+                        finish();
+                    }
+                })
+                .setNegativeButton("Hủy", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                })
+                .show();
+    }
+}
